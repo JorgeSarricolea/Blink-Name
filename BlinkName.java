12f8d43366f143c3c2f27b4840b0e05980e27dde
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-11-17 13:57:01 -0600

    build: hashmap to print color name

--- BlinkName.java
+++ BlinkName.java
@@ -1,8 +1,11 @@
 import java.awt.Color;
 import java.awt.Font;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlinkName {
 
@@ -11,15 +14,23 @@ public class BlinkName {
     public static final int COLOR_BLINK = 80; // Milliseconds
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> createAndShowGUI());
+    }
+
+    private static void createAndShowGUI() {
+        // GUI setup in the main thread
         JFrame frame = new JFrame("Blink Name");
         JLabel label = new JLabel(NAME);
         label.setFont(new Font("Arial", Font.PLAIN, TEXT_SIZE));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
         frame.getContentPane().add(label);
         frame.setSize(800, 200);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
 
+        // ColorChanger thread is started to change colors
         ColorChanger colorChanger = new ColorChanger(label);
         new Thread(colorChanger).start();
     }
@@ -35,7 +46,19 @@ class ColorChanger implements Runnable {
             Color.YELLOW
     };
 
+    private static final Map<Color, String> COLOR_NAMES = new HashMap<>();
+
+    static {
+        // Mapping colors to their names
+        COLOR_NAMES.put(Color.RED, "RED");
+        COLOR_NAMES.put(Color.GREEN, "GREEN");
+        COLOR_NAMES.put(Color.BLUE, "BLUE");
+        COLOR_NAMES.put(Color.MAGENTA, "MAGENTA");
+        COLOR_NAMES.put(Color.YELLOW, "YELLOW");
+    }
+
     private JLabel label;
+    private int[] colorIndex = { 0 }; // Use an array to store the index
 
     public ColorChanger(JLabel label) {
         this.label = label;
@@ -43,17 +66,36 @@ class ColorChanger implements Runnable {
 
     @Override
     public void run() {
-        int colorIndex = 0;
-
+        // Thread begins here
         while (true) {
             try {
+                // Get the name of the current thread
+                String threadName = Thread.currentThread().getName();
+
+                // Get current color
+                Color currentColor = COLORS[colorIndex[0]];
+
+                // Get color name
+                String colorName = COLOR_NAMES.get(currentColor);
+
+                // Print information about the thread in the terminal
+                System.out.println("Current Thread: " + threadName + " | Current Color: " + colorName);
+
+                // The thread is paused to change color at regular intervals
                 Thread.sleep(BlinkName.COLOR_BLINK);
 
-                label.setForeground(COLORS[colorIndex]);
+                // The user interface is updated
+                SwingUtilities.invokeLater(() -> {
+                    if (label != null) {
+                        label.setForeground(currentColor);
+                        label.repaint();
+                    }
+                });
 
-                colorIndex++;
-                if (colorIndex == COLORS.length) {
-                    colorIndex = 0;
+                // Changes to the next color
+                colorIndex[0]++;
+                if (colorIndex[0] == COLORS.length) {
+                    colorIndex[0] = 0;
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
